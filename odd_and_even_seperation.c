a234058aecfdb61db0908556488cb7838c3f0d43
Athul <laljiathul@gmail.com>
2025-01-01 13:10:01 +0530

    Modified odd even seperation code for keeping same order of array

--- odd_and_even_seperation.c
+++ odd_and_even_seperation.c
@@ -6,18 +6,22 @@ int main(){
 	int new_arr[size_of_arr];
 	int even_index = 0;
 	int odd_index = size_of_arr - 1;
-	for(int i = 0; i < size_of_arr; i++){
+	
+	for(int i = 0, j = size_of_arr - 1; i < size_of_arr; i++, j--){
 		if(arr[i] % 2 == 0){
 			new_arr[even_index] = arr[i];
 			even_index ++;
 		}
-		else{
-			new_arr[odd_index] = arr[i];
+		if(arr[j] % 2 != 0){
+			new_arr[odd_index] = arr[j];
 			odd_index --;
 		}
 	}
+
 	for(int j = 0; j < size_of_arr; j++){
 		printf("%d ", new_arr[j]);
 	}
+
+	printf("\n");
 	return 0;
 }
